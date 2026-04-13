dc9ea4cf3d1285018fd4a3a99f443cbd5679ed93
Яньшина Анастасия <nasyanshi@gmail.com>
2026-04-14 00:12:20 +0400

    относительно чистый код, но ещё без документации

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.dokka")
 }
 
 android {
@@ -62,4 +63,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+tasks.dokkaHtml {
+    outputDirectory.set(layout.buildDirectory.dir("dokka"))
 }
--- app/src/main/java/com/example/remindmind/Reminder.kt
+++ app/src/main/java/com/example/remindmind/Reminder.kt
@@ -1,34 +1,3 @@
-/*package com.example.remindmind
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
-
-enum class Priority {
-    HIGH,
-    MEDIUM,
-    LOW
-}
-
-data class SubTask(
-    val id: Int,
-    val text: String,
-    val date: String,
-    val time: String,
-    var isCompleted: Boolean = false,
-    val priority: Priority = Priority.MEDIUM
-)
-
-data class Reminder(
-    val id: Int,
-    val text: String,
-    val date: String,
-    val time: String,
-    var isCompleted: Boolean = false,
-    val priority: Priority = Priority.MEDIUM,
-    val subTasks: SnapshotStateList<SubTask> = mutableStateListOf()  // Изменяем тип
-)*/
-// Reminder.kt (обновленная версия)
 package com.example.remindmind
 
 import androidx.compose.runtime.snapshots.SnapshotStateList
